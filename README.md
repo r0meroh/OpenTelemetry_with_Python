29408bbc5810a55402c1d5cd71d899b281fbf82d
r0meroh <romeroh862@gmail.com>
2025-10-26 20:02:07 -0500

    Build: Added Otel resource config, tracer and internal fuction span

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OpenTelemetry_with_Python
+
+## This repository showcases a simple Python application's instrumentation
+
+### Installed OTEL dependencies
+[opentelemetry-api == 1.38.0](https://pypi.org/project/opentelemetry-api/)\
+[opentelemetry-sdk == 1.38.0](https://pypi.org/project/opentelemetry-sdk/)
--- app.py
+++ app.py
@@ -1,5 +1,56 @@
+import time
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.sdk.resources import Resource
+
+# Resource setup to specify service information
+myResourceConfig = Resource(attributes={
+    "service.name": "addition_service",
+    "originated_from": "hugos_machine",
+    "Cluster_Name": "Test_Cluster",
+    "Environment": "Development"
+})
+
+
+# OTEL Tracing setup
+provider = TracerProvider(resource=myResourceConfig)
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+
+# set the global tracer provider
+trace.set_tracer_provider(provider)
+
+# create a tracer from the global tracer provider
+myTracer = trace.get_tracer("myTracer")
+
 # simple python definition of a function that adds two numbers
+@myTracer.start_as_current_span("add_numbers_span")
 def add_numbers(first, second) -> None:
+    # add span status
+    current_span = trace.get_current_span()
+    current_span.set_status(trace.StatusCode.OK)
+    # add span attributes
+    current_span.set_attributes(attributes={
+        "first_number": first,
+        "second_number": second
+    })
+
+    # add a sleep function to simulate a delay in a span
+    # the code below affects the span duration and can be visible in start_time and end_time
+    time.sleep(2)
+    current_span.add_event(
+        name="sleeping for 2 seconds",
+        attributes={
+            "duration": 2,
+            "additional_message": "complete"
+            },
+        timestamp=time.time())
+    
+
     return first + second
 
 if __name__ == "__main__":
