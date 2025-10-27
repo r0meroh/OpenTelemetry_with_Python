5b552d8941fb53a068d1ed191166cd3eccefc91c
r0meroh <romeroh862@gmail.com>
2025-10-26 19:19:33 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+# simple python definition of a function that adds two numbers
+def add_numbers(first, second) -> None:
+    return first + second
+
+if __name__ == "__main__":
+    result = add_numbers(3, 5)
+    print(f"The sum of 3 and 5 is: {result}")   
